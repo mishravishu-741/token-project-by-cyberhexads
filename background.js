0757dab597837e033aa90b09707f49374a341629
mishravishu-741 <mishravishu741@gmail.com>
2024-10-15 21:12:16 +0530

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('YouTube Token Extension installed');
-  });
-  
-  chrome.storage.sync.get('token', (data) => {
-    console.log('Token:', data.token);
-  });
-  
